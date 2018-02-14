b5091a7d7ef1c0316b7579ebd725b59ab952df66
sebas <sebas@DESKTOP-KOL74N0.local>
2018-02-14 10:18:00 -0500

    Création du test dans le main frame

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -16,6 +16,18 @@ public class TestGit {
      */
     public static void main(String[] args) {
         System.out.println("Je suis senser travailler sur une seconde branch");
+        
+        Integer[] t = new Integer[10];
+        
+        for(int i = 0; i < t.length; i++){
+            
+            t[i] = i + 2;
+        }
+        
+        for(int i = 0; i < t.length ; i++){
+            
+            System.out.println("Un nombre : " + t[i]);
+        }
     }
     
 }
